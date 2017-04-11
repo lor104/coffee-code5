ed269eadab2c9fb66af775fabee243053704d54e
lor104 <lauren.rosentzveig@gmail.com>
2017-04-11 09:30:02 -0400

    finish ex2

--- ex2.rb
+++ ex2.rb
@@ -5,11 +5,25 @@
 
 
 def valid_phone_number(str)
-
+  phone = str.split(//)
+  # puts phone
+  if phone[0] == "(" && phone[4] == ")" && phone[5] == " " && phone[9] == "-" && phone.length == 14
+    phone.delete_at(9)
+    phone.delete_at(5)
+    phone.delete_at(4)
+    phone.delete_at(0)
+    phone.each do |num|
+      if num.is_a? Integer
+      end
+      return true
+    end
+  else
+    return false
+  end
 end
 
 
 
-valid_phone_number("(123) 456-7890") # => returns true
-valid_phone_number("(1111)555 2345") # => returns false
-valid_phone_number("(098) 123 4567") # => returns false
+puts valid_phone_number("(123) 456-7890") # => returns true
+puts valid_phone_number("(1111)555 2345") # => returns false
+puts valid_phone_number("(098) 123 4567") # => returns false
